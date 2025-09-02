70b82543a986938d9ef7143e2466b2be6f39af1a
MatiasHauria <hauriatute@gmail.com>
2025-09-02 13:23:53 -0300

    "Se agrega el codigo de Contacto"

--- src/tp5_gui/Contacto.java
+++ src/tp5_gui/Contacto.java
@@ -1,6 +1,68 @@
 package tp5_gui;
 
 public class Contacto {
+    private int DNI;
+    private String nombre;
+    private String apellido;
+    private String ciudad;
+    private String direccion;
+
+    public Contacto(int DNI, String nombre, String apellido, String ciudad, String direccion) {
+        this.DNI = DNI;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.ciudad = ciudad;
+        this.direccion = direccion;
+    }
+
+    public int getDNI() {
+        return DNI;
+    }
+
+    public void setDNI(int DNI) {
+        this.DNI = DNI;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    @Override
+    public String toString() {
+        return "nombre:" + nombre;
+    }
+    
+    
+    
+    
     
     
 }
