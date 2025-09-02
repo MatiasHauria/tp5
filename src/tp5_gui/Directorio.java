80ae0827c2ceaf1e137505599855fba1949407bd
MatiasHauria <hauriatute@gmail.com>
2025-09-02 13:14:08 -0300

    "Se agregan los metodos y el TreeMap en  Directorio"
    Se utiliza TreeMap a secas.

--- src/tp5_gui/Directorio.java
+++ src/tp5_gui/Directorio.java
@@ -4,10 +4,57 @@
  */
 package tp5_gui;
 
+import java.util.ArrayList;
+import java.util.TreeMap;
+
 /**
  *
  * @author Hernan
  */
 public class Directorio {
-    
+
+    TreeMap<Long, Contacto> lista = new TreeMap<>();
+
+    public Directorio() {
+    }
+
+    public void agregarContacto(Long nroTelefono, Contacto contacto) {
+        if (!lista.containsKey(nroTelefono)) {
+            lista.put(nroTelefono, contacto);
+        }
+    }
+
+    public Contacto buscarContacto(Long nroTelefono) {
+        if (lista.containsKey(nroTelefono)) {
+            return lista.get(nroTelefono);
+        }
+        return null;
+    }
+
+    public Long buscarTelefono(String apellido) {
+        for (Long numeros : lista.keySet()) {
+            Contacto auxiliar = lista.get(numeros);
+            if (auxiliar.getApellido().equalsIgnoreCase(apellido)) {
+                return numeros;
+            }
+        }
+        return null;
+    }
+
+    public Contacto buscarContactos(String ciudad) {
+        for (Contacto contactos : lista.values()) {
+            ArrayList<Contacto> listafinal = new ArrayList<>();
+            listafinal.add(contactos);
+            for(Contacto aux: listafinal){
+                if(aux.getCiudad().equalsIgnoreCase(ciudad))
+                    return aux;
+            }
+        }
+        return null;
+    }
+    public void borrarContacto(Long nroTelefono){
+        if(lista.containsKey(nroTelefono)){
+            lista.remove(nroTelefono);
+        }
+    }
 }
